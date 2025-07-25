7b26a2222d09a0ad9bdf4f7575647f8c9e9ba032
Thomas Ulmer <thomasmulmer02@gmail.com>
2025-07-24 20:14:14 -0700

    WIP parsing still broken,
    fixes hang on module load from bad recursive def.
    
    still doesn't seem to do the recursive defs correctly but at least it
    runs now

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,7 @@
 
 
+let () = print_endline "test top"
+
 (* TODO
  *
  * fix this to not use run_empty, since we should be threading the
@@ -7,28 +9,34 @@
  *
  *)
 let get_next_expr () =
+  let _ = Parse.Parse.Parser.empty_context in
   let e = Parse.Parse.parse_expression in
-  match Parse.Parse.Parser.run_empty e with
-  | Ok v ->
+  match e Parse.Parse.Parser.empty_context with
+  | Ok (v, _) ->
     let s = Cst.pp_expr v in
     print_string "CST ";
     print_endline s;
     v
-  | Error () -> raise @@ Failure "Failed to parse expression"
+  | Error _ -> raise @@ Failure "Failed to parse expression"
 
-let process cst_e =
-  let (_, ast_e) = Ast.from_cst [] [] cst_e in
-  print_string "AST ";
-  print_endline (Ast.pp_expr ast_e);
-  let open Compile.Compiler in
-  let c_action = compile ast_e in
-  let ops = run_empty c_action in
-  let s = Dynarray.fold_left (fun acc op -> acc ^ Bytecode.BC.pretty_print_op op ^ "\n") "Compiled\n" ops in
-  print_string s
+let _ = get_next_expr ()
 
-let run1 () =
-  print_endline "Debugging...";
-  let e = get_next_expr () in
-  process e
-
-let () = run1 ()
+let _ = print_endline "ended"
+(*
+ * let process cst_e =
+ *   let (_, ast_e) = Ast.from_cst [] [] cst_e in
+ *   print_string "AST ";
+ *   print_endline (Ast.pp_expr ast_e);
+ *   let open Compile.Compiler in
+ *   let c_action = compile ast_e in
+ *   let ops = run_empty c_action in
+ *   let s = Dynarray.fold_left (fun acc op -> acc ^ Bytecode.BC.pretty_print_op op ^ "\n") "Compiled\n" ops in
+ *   print_string s
+ * 
+ * let run1 () =
+ *   print_endline "Debugging...";
+ *   let e = get_next_expr () in
+ *   process e
+ * 
+ * let () = run1 ()
+ *)
