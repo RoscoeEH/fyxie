f3b84e5fd91d2582b60759ba7f2a58c1350aced6
Thomas Ulmer <thomasmulmer02@gmail.com>
2025-07-25 22:23:42 -0700

    ast pp, naming consistency, enable bytecode printing

--- bin/main.ml
+++ bin/main.ml
@@ -1,13 +1,5 @@
 
 
-let () = print_endline "test top"
-
-(* TODO
- *
- * fix this to not use run_empty, since we should be threading the
- * monad through and/or change it to not be a delayed computation.
- *
- *)
 let get_next_expr () =
   let _ = Parse.Parse.Parser.empty_context in
   let e = Parse.Parse.parse_expression in
@@ -16,27 +8,25 @@ let get_next_expr () =
     let s = Cst.pp_expr v in
     print_string "CST ";
     print_endline s;
+    print_endline "";
     v
   | Error _ -> raise @@ Failure "Failed to parse expression"
 
-let _ = get_next_expr ()
+let process cst_e =
+  let (_, ast_e) = Ast.from_cst [] [] cst_e in
+  print_string "AST ";
+  print_endline (Ast.pp_expr ast_e);
+  print_endline "";
+  let open Compile.Compiler in
+  let c_action = compile ast_e in
+  let ops = run_empty c_action in
+  let strs = Dynarray.mapi (fun i op -> string_of_int i ^ " " ^ Bytecode.BC.pp_op op ^ "\n") ops in
+  let s = Dynarray.fold_left (^) "Compiled\n" strs in
+  print_string s
 
-let _ = print_endline "ended"
-(*
- * let process cst_e =
- *   let (_, ast_e) = Ast.from_cst [] [] cst_e in
- *   print_string "AST ";
- *   print_endline (Ast.pp_expr ast_e);
- *   let open Compile.Compiler in
- *   let c_action = compile ast_e in
- *   let ops = run_empty c_action in
- *   let s = Dynarray.fold_left (fun acc op -> acc ^ Bytecode.BC.pretty_print_op op ^ "\n") "Compiled\n" ops in
- *   print_string s
- * 
- * let run1 () =
- *   print_endline "Debugging...";
- *   let e = get_next_expr () in
- *   process e
- * 
- * let () = run1 ()
- *)
+let run1 () =
+  print_endline "Debugging...";
+  let e = get_next_expr () in
+  process e
+
+let () = run1 ()
